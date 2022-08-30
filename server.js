2011eb34f34dd6c886330ae76f8cfd802b736994
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-29 21:26:46 -0400

    Update

---
+++ server.js
@@ -0,0 +1,18 @@
+// importing the express module
+const express = require("express");
+
+// create an instance of express (create the server)
+const app = express();
+
+// allows frontend to server static files
+app.use(express.static("public"));
+
+// create a route for the root path
+app.get("/test", function (req, res) {
+  res.send("Hello World");
+});
+
+// sets the port to listen on
+app.listen(3000, () => {
+  console.log("Server Listening on port 3000");
+});
